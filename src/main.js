754b44a0e408f3b2d1e7bd0ebcfe4cedbc722911
NTDungxPH28795 <dungndtph28795@fpt.edu.vn>
2023-05-24 17:42:53 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+import classes from "./main.module.css";
+
+document.querySelector('#app').innerHTML = `
+
+    <div class="${classes.red}">
+    hello world
+    </div>
+`
